85cf6daf2d656c04e825ead2fd442701e379abf0
ayu-sharma <contactayusharma@gmail.com>
2025-09-07 14:09:11 +0530

    update this

--- app/transport/page.js
+++ app/transport/page.js
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const TransportPage = () => {
+  return (
+    <div>
+      <h1>Transport Page</h1>
+      <p>This is a static transport page.</p>
+    </div>
+  );
+};
+
+export default TransportPage;
